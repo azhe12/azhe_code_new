97af74b9534d6927e5ab57b24fd1d31409e3ef68
azhe_liu <azhe>
2013-09-04 20:47:03 +0800

    [show-idential-file.sh]

---
+++ misc/show-idential-file.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#Usage: show-idential-file.sh files
+
+IFS='
+	'
+md5sum "$@" /dev/null 2>/dev/null |
+awk '{
+		count[$1]++
+		if (count[$1] == 1) first[$1] = $0
+		if (count[$1] == 2) print first[$1]
+		if (count[$1] > 1) print $0
+}' |
+	sort  |
+		awk '{
+			if (last != $1) print ""
+				last = $1
+				print $0
+		}'
