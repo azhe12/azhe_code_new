159000b71faa97428e3f03cb136db08c56dc4c37
azhe_liu <azhe>
2013-09-23 08:48:11 +0800

    [MergeSort] complete

--- summary/programmingPearls/chapter_01/mergeSort.c
+++ summary/programmingPearls/chapter_01/mergeSort.c
@@ -1,33 +1,74 @@
+/*merge sort
+ * azhe liu 2013.9.22
+ * Read source file from STDIN
+ * Output sorted data to STDOUT
+ * Usage: cat sourcefile | ./mergeSort
+ *
+ * */
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-void mergeSortedNumber(unsigned int * a_left, unsigned int size_left, \
-		unsigned int * a_right, unsigned int size_right, unsigned int * result)
+#define ARRAY_SIZE	10000000
+#ifdef DEBUG
+	#define pr_debug(...) printf(...)
+#else
+	#define pr_debug(...)
+#endif
+
+void mergeSortedNumber(unsigned int * a, unsigned int first, unsigned int mid, unsigned int end, unsigned int *tmp)
 {
-	unsigned int i = 0, j = 0, k = 0;
-	while (i < size_left && j < size_right) {
-		if (a_left[i] <= a_right[j])
-			result[k++] = a_left[i++];
+	//unsigned int * tmp = malloc(ARRAY_SIZE * sizeof(int));
+	unsigned int i = first, j = mid + 1, k = 0;
+	while ((i <= mid ) && (j <= end )) {
+		if (a[i] <= a[j])
+			tmp[k++] = a[i++];
 		else
-			result[k++] = a_right[j++];
+			tmp[k++] = a[j++];
 	}
-	if (i >= size_left)
-		while (j < size_right)
-			result[k++] = a_right[j++];
-	else if (j >= size_right)
-		while (i < size_left)
-			result[k++] = a_left[i++];
+		while (i <= mid)
+			tmp[k++] = a[i++];
+		while (j <= end)
+			tmp[k++] = a[j++];
+	for (i = 0; i < k; i++) {
+		a[first + i] = tmp[i];
+		pr_debug("%d\n", tmp[i]);
+	}
+	pr_debug(">>>>\n");
 }
 
-#define ARRAY_SIZE	10000000
+void mergesort(unsigned int *a, unsigned int bottom, unsigned int top, unsigned int* tmp)
+{
+	unsigned int mid;
+	if (top > bottom) {
+		mid = (bottom + top) / 2;
+		mergesort(a, bottom, mid, tmp);
+		mergesort(a, mid + 1, top, tmp);
+		//mergeSortedNumber(a + bottom, mid - bottom + 1, a + mid + 1, top - mid, tmp);
+		mergeSortedNumber(a, bottom, mid, top, tmp);
+	}
+}
+
+void MergeSort(unsigned int* a, unsigned int size)
+{
+	unsigned int * tmp;
+	if ( !(tmp = malloc(ARRAY_SIZE * sizeof(int)))) {
+		printf("no memory!\n");
+		return;
+	}
+	mergesort(a, 0, size - 1, tmp);
+	free(tmp);
+}
 int main(int argc, char** argv)
 {
-	unsigned int n, i = 0;
+	unsigned int n, i = 0, size;
 	unsigned int *a = malloc(sizeof(int) * ARRAY_SIZE);
 	while(scanf("%d", &n) != EOF) {
 		a[i++] = n;
-		printf("input num = %d\n", a[i - 1]);
+		pr_debug("input num = %d\n", a[i - 1]);
 	}
-
+	size = i;
+	MergeSort(a, size);
+	for (i = 0; i < size; i++)
+		printf("%d\n", a[i]);
 	free(a);
 }
