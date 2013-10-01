c3937fd1a6dee8ba26efc0670d8da1d6d0995650
azhe_liu <liuyuanzhe123@126.com>
2013-10-01 19:11:50 +0800

    [programmingPearls][chapter_02]  cycle shift (finish)

--- summary/programmingPearls/chapter_02/cycleShift.c
+++ summary/programmingPearls/chapter_02/cycleShift.c
@@ -5,24 +5,50 @@
  * arg1: shift
  * Usage: cat sourcefile|./cycleShift 3
  * */
+#define DEBUG
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 #include "baseLib.h"
 #define MAX_LEN	1000
 int main(int argc, char **argv)
 {
 	uint32 *a = malloc(sizeof(uint32) * MAX_LEN);
-	uint32 input_nums;
-	int shift, i;
+	uint32 input_nums, t, left, right;
+	int shift, i, k, start;
+#if 1
 	if (!argv[1]) {
 		printf("arg1 must be shift\n");
 		return -1;
 	}
 	shift = atoi(argv[1]);
 	printf("shift:%d\n", shift);
-
+#endif
 	input_nums = readStdinNumToArray(a, sizeof(uint32) * MAX_LEN);
+#if 0
 	for (i = 0; i < input_nums; i++)
-		printf("a[%d]= %u", i, a[i]);
+		printf("a[%d]= %u\n", i, a[i]);
+#endif
+	i = 0;
+	start = 0;
+	k = 0;
+	while (k < input_nums) {
+		if (i == 0)
+			t = a[start];
+		left = start + i * shift;
+		right = (left + shift) % input_nums;
+		/*cycle shift*/
+		if (right == start) {
+			start++;
+			i = 0;
+			a[left] = t;
+		} else {
+			a[left] = a[right];
+			i++;
+		}
+		k++;
+	}
+	for (i = 0; i < input_nums; i++)
+		printf("a[%d]= %u\n", i, a[i]);
 	return 0;
 }
