f398d880037085cea34d2e660c595a307f59686e
azhe_liu <azhe_liu@htc.com>
2013-11-18 14:45:01 +0800

    [timer] linux timer, precision of timer_signal is higher

---
+++ misc/timer.c
@@ -0,0 +1,23 @@
+#include  <sys/select.h>
+/*#include  <sys/timer.h>*/
+#include  <sys/time.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <signal.h>
+int n = 0;
+void timefunc(int sig)      /* 定时事件代码 */
+{
+fprintf(stderr, "ITIMER_PROF[%d]\n", n++);
+signal(SIGPROF, timefunc);    /* 捕获定时信号 */
+}
+void main()
+{
+struct itimerval value;
+value.it_value.tv_sec=1;    /* 定时1.5秒 */
+value.it_value.tv_usec=500000;
+value.it_interval.tv_sec=0;    /* 定时1.5秒 */
+value.it_interval.tv_usec=1000;
+signal(SIGPROF, timefunc);     /* 捕获定时信号 */
+setitimer(ITIMER_PROF, &value, NULL); /* 定时开始 */
+while (1);
+}
