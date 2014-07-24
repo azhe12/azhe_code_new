4d4eb1ea8bf6419d9f54ff6b0d3eaf027225b0cf
azhe_liu <liuyuanzhe123@126.com>
2014-07-24 15:02:45 +0800

    remove summary dir

--- summary/android_study/driver_code/hello/.tmp_versions/hello.mod
+++
@@ -1,2 +0,0 @@
-/home/azhe/share/win7/azhe_code/summary/driver_code/hello/hello.ko
-/home/azhe/share/win7/azhe_code/summary/driver_code/hello/hello.o
--- summary/presentation/subsystem/tty_console/tty_test/driver/.tty_azhe.o.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/azhe/other/ldd_azhe/tty/driver/tty_azhe.o := arm-eabi-ld    -r -o /home/azhe/other/ldd_azhe/tty/driver/tty_azhe.o /home/azhe/other/ldd_azhe/tty/driver/main.o 
